b4841b1a05368e9b47e40a64d5647c02980947d8
Mathieu Botter <mathieubotter@outlook.com>
2023-12-04 23:29:07 +0100

    Improve day 1

--- lib/advent_of_code/year_2023/day_01.ex
+++ lib/advent_of_code/year_2023/day_01.ex
@@ -1,5 +1,6 @@
 defmodule AdventOfCode.Year2023.Day01 do
   @digits ~w(1 2 3 4 5 6 7 8 9)
+  @spelled_out_digits ~w(one two three four five six seven eight nine)
 
   def part1(input) do
     parse(input)
@@ -24,24 +25,14 @@ defmodule AdventOfCode.Year2023.Day01 do
     do: digit(line, :forward) <> digit(String.reverse(line), :reverse)
 
   defp digit("", _), do: "0"
-  defp digit("one" <> _rest, :forward), do: "1"
-  defp digit("two" <> _rest, :forward), do: "2"
-  defp digit("three" <> _rest, :forward), do: "3"
-  defp digit("four" <> _rest, :forward), do: "4"
-  defp digit("five" <> _rest, :forward), do: "5"
-  defp digit("six" <> _rest, :forward), do: "6"
-  defp digit("seven" <> _rest, :forward), do: "7"
-  defp digit("eight" <> _rest, :forward), do: "8"
-  defp digit("nine" <> _rest, :forward), do: "9"
-  defp digit("eno" <> _rest, :reverse), do: "1"
-  defp digit("owt" <> _rest, :reverse), do: "2"
-  defp digit("eerht" <> _rest, :reverse), do: "3"
-  defp digit("ruof" <> _rest, :reverse), do: "4"
-  defp digit("evif" <> _rest, :reverse), do: "5"
-  defp digit("xis" <> _rest, :reverse), do: "6"
-  defp digit("neves" <> _rest, :reverse), do: "7"
-  defp digit("thgie" <> _rest, :reverse), do: "8"
-  defp digit("enin" <> _rest, :reverse), do: "9"
+
+  for {spelled_out, index} <- Enum.with_index(@spelled_out_digits, &{&1, &2 + 1}) do
+    defp digit(unquote(spelled_out) <> _rest, :forward), do: unquote(to_string(index))
+
+    defp digit(unquote(String.reverse(spelled_out)) <> _rest, :reverse),
+      do: unquote(to_string(index))
+  end
+
   defp digit(<<d::binary-size(1), _rest::binary>>, _) when d in @digits, do: d
   defp digit(<<_d::binary-size(1), rest::binary>>, spelled_out), do: digit(rest, spelled_out)
 end
